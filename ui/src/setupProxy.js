f5854ba6f04ddbeb9a73abe37da9abc960a1ee85
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-11-30 14:24:27 +0100

    Fixed license and excluded some files from license check.

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
 let jsonApiUrl = process.env.REACT_APP_JSON_API_URL;
