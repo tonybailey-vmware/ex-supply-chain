fde3aacaf26a99dd00f4dc4f9c3b5036459eb055
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2021-05-18 18:58:09 +0200

    remove commented code

--- ui/src/generateParties.js
+++ ui/src/generateParties.js
@@ -1,21 +1,6 @@
-const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
-// var stream = fs.createWriteStream("parties.json");
-
 const participantNames = ["buyer", "seller", "supplier", "warehouse1", "warehouse2", "transportCompany1", "transportCompany2"];
-
-// stream.once('open', function() {
-//   stream.write(JSON.stringify(Object.fromEntries(Object.entries(participants.party_participants)
-//   .filter(participant =>
-//         desiredParties.includes(participant[1]))
-//   .map(participant =>
-//         [participant[1], participant[0]])
-//   )));
-//   stream.end();
-// });
-
-
 
 function generate(participantNames) {
     const ledgerId = "demo";
