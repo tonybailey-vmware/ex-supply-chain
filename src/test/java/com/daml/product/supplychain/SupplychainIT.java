bb80b63b1719c15346b14b577f47451f2fa9c0c3
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2021-05-27 10:22:16 +0200

    use launcher script in Java test (#135)
    * use launcher script in Java test
    
    * fix launcher script for triggers
    
    * install pipenv dependencies before mvn verifiy
    
    * remove obsolete comment

--- src/test/java/com/daml/product/supplychain/SupplychainIT.java
+++ src/test/java/com/daml/product/supplychain/SupplychainIT.java
@@ -94,8 +94,7 @@ public class SupplychainIT {
     File errLog = new File("integration-triggers.err.log");
     triggers =
         new ProcessBuilder()
-            // need to call Python directly for proper subprocess cleanup (not sure why though)
-            .command("scripts/startTriggers.py", Integer.toString(sandbox.getSandboxPort()))
+            .command("launchers/automation", Integer.toString(sandbox.getSandboxPort()))
             .redirectOutput(ProcessBuilder.Redirect.appendTo(log))
             .redirectError(ProcessBuilder.Redirect.appendTo(errLog))
             .start();
